f446192b51508e67411330258d6c6e7d793b0674
Levchenko Nikita <n.levchenko@everypixel.ru>
2023-02-08 15:12:02 +0500

    base version

--- main.py
+++ main.py
@@ -52,41 +52,6 @@ def video_to_frames(input_dir, output_dir):
             cv2.imwrite(os.path.join(output_video_dir, file_name.zfill(9)), frame)
             ###################
 
-
-
-
-    # videoCapture = cv2.VideoCapture()
-    # videoCapture.open(path)
-    # fps = videoCapture.get(cv2.CAP_PROP_FPS)
-    # frames = videoCapture.get(cv2.CAP_PROP_FRAME_COUNT)
-    # print("fps=", int(fps), "frames=", int(frames))
-    #
-    # dir_number = -1;
-    # dir_name = ""
-    # for i in range(int(frames)):
-    #     ret, frame = videoCapture.read()
-    #     file_name = str("%d.png" % (i))
-    #     print(file_name.zfill(9), "/%d" % frames)
-    #
-    #     ###################
-    #     frame = cv2.resize(frame, (int(frame.shape[1] / 4), int(frame.shape[0] / 4)))
-    #     ###################
-    #
-    #     ###################
-    #     if i >= (dir_number + 1) * 40:
-    #         dir_number += 1
-    #         dir_name = str(dir_number).zfill(3)
-    #         os.mkdir("/home/jasmine/Framing/frames/" + dir_name)
-    #     cv2.imwrite("/home/jasmine/Framing/frames/" + dir_name + "/" + file_name.zfill(9), frame)
-    #     ###################
-    #
-    #
-    #
-    #     ###################
-    #     # cv2.imwrite("/home/jasmine/Framing/frames2/" + file_name.zfill(9), frame)
-    #     ###################
-
-
 if __name__ == '__main__':
     time_start = datetime.now()
 
