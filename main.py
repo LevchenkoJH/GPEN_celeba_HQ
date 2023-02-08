453f534bbe2527277481980d4eec5ed8f013b4d2
Levchenko Nikita <n.levchenko@everypixel.ru>
2023-02-08 15:11:16 +0500

    base version

--- main.py
+++ main.py
@@ -1,42 +1,110 @@
 import cv2
 import os
 from datetime import datetime
+import argparse
+from tqdm import tqdm
 
-def video_to_frames(path):
-    videoCapture = cv2.VideoCapture()
-    videoCapture.open(path)
-    fps = videoCapture.get(cv2.CAP_PROP_FPS)
-    frames = videoCapture.get(cv2.CAP_PROP_FRAME_COUNT)
-    print("fps=", int(fps), "frames=", int(frames))
+def video_to_frames(input_dir, output_dir):
+    dirs = sorted(os.listdir(input_dir))
+    print("input_dir", input_dir)
+    print(dirs)
 
-    dir_number = -1;
-    dir_name = ""
-    for i in range(int(frames)):
-        ret, frame = videoCapture.read()
-        file_name = str("%d.png" % (i))
-        print(file_name.zfill(9), "/%d" % frames)
+    for video_path  in tqdm(dirs):
+        print("video_path ->", video_path)
+        video_path_tmp = os.path.join(input_dir, video_path)
+        # print("video_path_tmp ->", video_path_tmp)
 
-        ###################
-        frame = cv2.resize(frame, (int(frame.shape[1] / 4), int(frame.shape[0] / 4)))
-        ###################
+        videoCapture = cv2.VideoCapture()
+        videoCapture.open(video_path_tmp)
+        # ФПС и суммарное количество кадров
+        fps = videoCapture.get(cv2.CAP_PROP_FPS)
+        frames = videoCapture.get(cv2.CAP_PROP_FRAME_COUNT)
+        print("fps=", int(fps), "frames=", int(frames))
 
-        ###################
-        if i >= (dir_number + 1) * 40:
-            dir_number += 1
-            dir_name = str(dir_number).zfill(3)
-            os.mkdir("/home/jasmine/Framing/frames/" + dir_name)
-        cv2.imwrite("/home/jasmine/Framing/frames/" + dir_name + "/" + file_name.zfill(9), frame)
-        ###################
+        # dir_number = -1;
+        # dir_name = ""
+
+        output_video_dir = os.path.join(output_dir, video_path)
+        os.makedirs(output_video_dir)
+
+        for i in range(int(frames)):
+            ret, frame = videoCapture.read()
+            # Название кадра
+            file_name = str("%d.png" % (i))
+            # print(file_name.zfill(9), "/%d" % frames)
+
+            ###################
+            # Если нужно уменьшить кадр
+            # frame = cv2.resize(frame, (int(frame.shape[1] / 4), int(frame.shape[0] / 4)))
+            ###################
+
+            ###################
+            # Записать видео разбив его по папкам, в данном случае по 40 кадров в каждой
+            # if i >= (dir_number + 1) * 40:
+            #     dir_number += 1
+            #     dir_name = str(dir_number).zfill(3)
+            #     os.mkdir("/home/jasmine/Framing/frames/" + dir_name)
+            # cv2.imwrite("/home/jasmine/Framing/frames/" + dir_name + "/" + file_name.zfill(9), frame)
+            ###################
+
+            ###################
+            # Все кадры в одной папке / название папки соответствует названию видео
+            cv2.imwrite(os.path.join(output_video_dir, file_name.zfill(9)), frame)
+            ###################
 
 
 
-        ###################
-        # cv2.imwrite("/home/jasmine/Framing/frames2/" + file_name.zfill(9), frame)
-        ###################
+
+    # videoCapture = cv2.VideoCapture()
+    # videoCapture.open(path)
+    # fps = videoCapture.get(cv2.CAP_PROP_FPS)
+    # frames = videoCapture.get(cv2.CAP_PROP_FRAME_COUNT)
+    # print("fps=", int(fps), "frames=", int(frames))
+    #
+    # dir_number = -1;
+    # dir_name = ""
+    # for i in range(int(frames)):
+    #     ret, frame = videoCapture.read()
+    #     file_name = str("%d.png" % (i))
+    #     print(file_name.zfill(9), "/%d" % frames)
+    #
+    #     ###################
+    #     frame = cv2.resize(frame, (int(frame.shape[1] / 4), int(frame.shape[0] / 4)))
+    #     ###################
+    #
+    #     ###################
+    #     if i >= (dir_number + 1) * 40:
+    #         dir_number += 1
+    #         dir_name = str(dir_number).zfill(3)
+    #         os.mkdir("/home/jasmine/Framing/frames/" + dir_name)
+    #     cv2.imwrite("/home/jasmine/Framing/frames/" + dir_name + "/" + file_name.zfill(9), frame)
+    #     ###################
+    #
+    #
+    #
+    #     ###################
+    #     # cv2.imwrite("/home/jasmine/Framing/frames2/" + file_name.zfill(9), frame)
+    #     ###################
 
 
 if __name__ == '__main__':
-    t1 = datetime.now()
-    video_to_frames("videos/2.mp4")
-    t2 = datetime.now()
-    print("Time cost = ", (t2 - t1))
+    time_start = datetime.now()
+
+    parser = argparse.ArgumentParser()
+
+
+    # Набор видео из датасета
+    parser.add_argument('--input_dir', type=str, default='./videos')
+    # Кадры
+    parser.add_argument('--output_dir', type=str, default='./frames')
+
+    args = parser.parse_args()
+
+    input_dir = args.input_dir
+    output_dir = args.output_dir
+
+    video_to_frames(input_dir, output_dir)
+
+
+    time_end = datetime.now()
+    print("Time cost = ", (time_end - time_start), "seconds")
